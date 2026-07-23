01a84908e484a2984c9c2fb29a79e4a9b1acb416
Alexis MABANZA <alexis.mabanza@effinnov.com>
2026-07-23 12:02:38 +0200

    fix(photos): comment out missing images to fix build

--- src/components/photos/PhotoHelloTokyo2.vue
+++ src/components/photos/PhotoHelloTokyo2.vue
@@ -3,9 +3,9 @@ import type { Photo } from '../../../photos/data'
 import raw from '../../../photos/data'
 
 const photos: Photo[] = [
-  'p-2025-03-18-23-32-18-000-1',
-  'p-2025-03-18-23-38-45-000-1',
-  'p-2025-03-18-23-39-20-000-1',
+  // 'p-2025-03-18-23-32-18-000-1',
+  // 'p-2025-03-18-23-38-45-000-1',
+  // 'p-2025-03-18-23-39-20-000-1',
 ].map(name => raw.find(p => p.url.includes(name))!)
 </script>
 
